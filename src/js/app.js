88921aafd19c150c1f3d0855f10dbbfcafe57129
Míriam Herrera Blanes <mherrerabl@uoc.edu>
2023-06-25 02:21:30 +0200

    Modified v43 detail

--- src/js/app.js
+++ src/js/app.js
@@ -108,8 +108,8 @@ $(function(){
     //Rep l'id del enllaç selecionat i l'emmagatzema en localStorage
     let categoryClicked = localStorage.getItem("category");
     categoryClicked === "" ? categoryClicked = "architecture" : categoryClicked = categoryClicked;
-    let detailClicked = "";
-    //categoryClicked = ""
+    let detailClicked = "arch1";
+    categoryClicked = "architecture"
     setDetail(".card a");
     setDetail(".navBigScreen a");
 
